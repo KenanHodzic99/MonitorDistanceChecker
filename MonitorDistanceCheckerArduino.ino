90897cddf383bf4ab67955cc931399c28089c6f5
KenanHodzic99 <48251483+KenanHodzic99@users.noreply.github.com>
2022-02-03 16:59:27 +0100

    Update MonitorDistanceCheckerArduino.ino

--- MonitorDistanceCheckerArduino.ino
+++ MonitorDistanceCheckerArduino.ino
@@ -1,22 +1,23 @@
-#define echoPin 50 
-#define trigPin 52 
+#define echoPin 50
+#define trigPin 52
 
-long trajanje; 
-int daljina; 
+long duration;
+int distance;
 
-void setup() {
+void setup()
+{
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   Serial.begin(300);
 }
-void loop() {
+void loop()
+{
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  trajanje = pulseIn(echoPin, HIGH);
-  daljina = (trajanje - 10) * 0.034 / 2;
-  Serial.println(daljina);
-  
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration - 10) * 0.034 / 2;
+  Serial.println(distance);
 }
